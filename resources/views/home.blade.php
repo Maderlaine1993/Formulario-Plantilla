2fd9073f497082614728633cd6f32acfc9776dd7
Maderlaine1993 <maldanam5@miumg.edu.gt>
2021-09-29 23:23:51 -0600

    Formulario de plantillas con bootstrap y laravel

---
+++ resources/views/home.blade.php
@@ -0,0 +1,54 @@
+@extends('layouts.plantilla')
+
+@section('contend')
+    <div class="bg-success card mt-5 p-3 text-white">
+        <form>
+            <h1 class="text-center" > Registro de alumnos </h1>
+            <div class="form-row">
+                <div class="form-group col-md-6">
+                    <label for="inputEmail4">Correo</label>
+                    <input type="email" class="form-control" id="inputEmail4" placeholder="Email">
+                </div>
+                <div class="form-group col-md-6">
+                    <label for="inputPassword4">Contraseña</label>
+                    <input type="password" class="form-control" id="inputPassword4" placeholder="Password">
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="inputAddress">Nombre</label>
+                <input type="text" class="form-control" id="inputAddress" placeholder="Alumno">
+            </div>
+            <div class="form-group">
+                <label for="inputAddress2">Direccion</label>
+                <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
+            </div>
+            <div class="form-row">
+                <div class="form-group col-md-6">
+                    <label for="inputCity">Telefono</label>
+                    <input type="text" class="form-control" id="inputCity">
+                </div>
+                <div class="form-group col-md-4">
+                    <label for="inputState">Ciudad</label>
+                    <select id="inputState" class="form-control">
+                        <option selected>Choose...</option>
+                        <option>...</option>
+                    </select>
+                </div>
+                <div class="form-group col-md-2">
+                    <label for="inputZip">Zip</label>
+                    <input type="text" class="form-control" id="inputZip">
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="form-check">
+                    <input class="form-check-input" type="checkbox" id="gridCheck">
+                    <label class="form-check-label" for="gridCheck">
+                        Check me out
+                    </label>
+                </div>
+            </div>
+            <button type="submit" class="btn btn-primary">Sign in</button>
+        </form>
+    </div>
+@endsection
+
